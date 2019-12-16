4e3c89c6a50219e1454dc04162f3ff9f74fd7a8a
bagyoda <bagyondaniel@gmail.com>
2019-12-16 13:04:00 +0100

    init application

---
+++ src/main/java/com/vending/videocontrol/VideoControlApplication.java
@@ -0,0 +1,11 @@
+package com.vending.videocontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoControlApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(VideoControlApplication.class, args);
+  }
+}
---
+++ src/test/java/com/vending/videocontrol/VideoControlApplicationTest.java
@@ -0,0 +1,12 @@
+package com.vending.videocontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class VideoControlApplicationTest {
+
+  @Test
+  void contextLoads() {
+  }
+}
